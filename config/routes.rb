a1d92b0def363246ea15d550458eb3b7a383439a
Foo Tang <godfoo@gmail.com>
2023-11-01 00:00:32 -0500

    Added the new actions and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'customers#index'
+  resources :customers do
+    collection do
+      get 'alphabetized'
+      get 'missing_email'
+    end
+  end
+
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
